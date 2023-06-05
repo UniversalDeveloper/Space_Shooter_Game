0f167d8236f5ce319a9ed9561503c9897e091585
Anastasiia Lopata <lopata.ad@gmail.com>
2023-06-05 14:43:24 +0200

    created fire point of Player Ship which is moving

---
+++ Assets/Attack.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attack : MonoBehaviour
+{
+    public GameObject shotPref;
+    public Transform firePoint;
+    public float attackRate;
+    private float innerAttackRate = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (innerAttackRate>= attackRate)
+        {
+            Instantiate(shotPref,firePoint);
+            innerAttackRate = 0;
+        }
+        innerAttackRate += Time.deltaTime;
+    }
+}
---
+++ Assets/Shot.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shot : MonoBehaviour
+{
+    public int direction;
+    public float force;
+    
+    void Start()
+    {
+        FindObjectOfType<Rigidbody2D>().AddForce(Vector2.up*direction*force);
+    }
+
+}
